6c568e860c1c5cac61f0b4f68852392feeaf80e8
IKubaszI <123005268+IKubaszI@users.noreply.github.com>
2024-04-05 12:48:33 +0200

    Initial commit

---
+++ baze.js
@@ -0,0 +1,11 @@
+/*let users = [ 
+    
+    {"name": " Jakub", "email": "taktoemail@.com", "password": "12345678" },
+    {"name": " Szymon", "email": "taktoemail@.com", "password": "12345678" },
+    {"name": " NomySZ", "email": "taktoemail@.com", "password": "12345678" },
+    {"name": " Bukaj", "email": "taktoemail@.com", "password": "12345678" },
+]
+
+module.exports = users;
+
+*/
